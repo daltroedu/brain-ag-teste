80ba7d4d8f64c6929bad28bdffa487afa920d4d2
Eduardo Daltro <edaltro23@gmail.com>
2024-04-05 10:17:17 -0300

    Add to agro/urls.py: FarmViewSet, CropTypeViewSet, CropViewSet and DashboardAPIView

--- agro/urls.py
+++ agro/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'farmers', views.FarmerViewSet)
+router.register(r'farms', views.FarmViewSet)
+router.register(r'crops_type', views.CropTypeViewSet)
+router.register(r'crops', views.CropViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('dashboard/', views.DashboardAPIView.as_view(), name='dashboard'),
 ]
