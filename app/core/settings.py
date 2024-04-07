b38d4f722be52faedbcd4c3c6755dd9db7a0ccdb
Eduardo Daltro <edaltro23@gmail.com>
2024-04-07 20:13:03 -0300

    Change secret keys and gunicorn app wsgi

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "agro")
+SECRET_KEY = os.environ.get("SECRET_KEY", "core")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
